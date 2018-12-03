d97da6530968ee747a5f3b40f733b10ac52f4c2c
JanUrf <janurfei@gmail.com>
2018-12-03 18:57:24 +0100

    vorbereitung der datenstrukturen für alpha beta search
    utilites array für die einzelnen Zugmöglichkeiten hinzugefügt.
    
    speichern des eigenen Houses und des Gegnerischen. Kann zukünftig für simulation der bewertung genutzt werden.

--- src/main/scala/info/kwarc/teaching/AI/Kalah/WS1819/Agents/AgentHAJANI.java
+++ src/main/scala/info/kwarc/teaching/AI/Kalah/WS1819/Agents/AgentHAJANI.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import info.kwarc.teaching.AI.Kalah.Board;
 import info.kwarc.teaching.AI.Kalah.Agents.Agent;
+import scala.Tuple4;
 
 public class AgentHAJANI extends Agent {
 
@@ -35,19 +36,51 @@ public class AgentHAJANI extends Agent {
 
 	@Override
 	public int move() {
-		Iterable<Object> houses = board.getHouses(this);
-		int housecounter=1;
-		int maxvalue=0;
-		int maxindex=0;
-		for(Object house:houses) {
-			if((int)house>maxvalue) {
-				maxvalue=(int)house;
-				maxindex=housecounter;
-			}
-			housecounter++;
-		}
-		
-		return maxindex;
+		//states ist das komplette Board als (Board Player 1, Board Player 2, House Player 1, House Player 2)
+				Tuple4<Iterable<Object>,Iterable<Object>,Object,Object> states = board.getState();
+
+				//bestimmung der generischen Haeuser und der eigenen
+				Iterable<Object> ownHouses = board.getHouses(this);
+				if(playerOne) {
+					Iterable<Object> otherHouses = states._2();
+				}else {
+					Iterable<Object> otherHouses = states._1();
+				}
+				
+				
+				//Anzahl der Houses herausfinden, weiss im Moment nicht ob das klueger geht.
+				int numberOfHouses = 0;
+			    for (Object i : ownHouses) {
+			        numberOfHouses++;
+			    }
+			    
+			    //utilitie Array anlegen, wo nachher drin stehen soll, wie viel einem ein Zug von einem Haus bringt. 
+			    //davon sollte dann das maximum gewaehlt werden.
+				int[] utilities = new int[numberOfHouses];
+
+				
+				//voruebergehend um das Haus mit den meisten auszuwaehlen
+				for(int i =0;i<numberOfHouses;i++) {
+					utilities[i]=board.getSeed((playerOne?1:2), i+1);
+				}
+
+
+				
+				//TODO utilities befuellen mit werten fue jeden moeglichen Zug
+				
+				
+				
+				
+				//bestimmen der besten Option
+				int maxvalue=0;
+				int maxindex=1;
+				for(int i=1;i<=numberOfHouses;i++) {
+					if(utilities[i-1]>maxvalue) {
+						maxvalue=utilities[i-1];
+						maxindex=i;
+					}
+				}
+				return maxindex;
 	}
 
 	@Override
---
+++ src/main/scala/info/kwarc/teaching/AI/Kalah/WS1819/Agents/AgentMaxHouseChooser.java
@@ -0,0 +1,59 @@
+package info.kwarc.teaching.AI.Kalah.WS1819.Agents;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import info.kwarc.teaching.AI.Kalah.Board;
+import info.kwarc.teaching.AI.Kalah.Agents.Agent;
+import scala.Tuple4;
+
+public class AgentMaxHouseChooser extends Agent {
+
+	private String name="AgentHAJANI";
+	private Board board;
+	private boolean playerOne;
+
+	public AgentMaxHouseChooser() {}
+	public AgentMaxHouseChooser(String name) {
+		this.name=name;
+	}
+	
+	@Override
+	public Iterable<String> students() {
+		List<String> studs=new ArrayList();
+		studs.add("Hanna");
+		studs.add("Jan Urf");
+		studs.add("Nicholas Dickel");
+		return studs;
+	}
+
+	@Override
+	public void init(Board board, boolean playerOne) {
+		this.board=board;
+		this.playerOne=playerOne;
+
+	}
+
+	@Override
+	public int move() {
+		Iterable<Object> houses = board.getHouses(this);
+		int housecounter=1;
+		int maxvalue=0;
+		int maxindex=0;
+		for(Object house:houses) {
+			if((int)house>maxvalue) {
+				maxvalue=(int)house;
+				maxindex=housecounter;
+			}
+			housecounter++;
+		}
+		
+		return maxindex;
+	}
+
+	@Override
+	public String name() {
+		return name;
+	}
+
+}
